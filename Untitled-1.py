d08517093ec9c9921fd3b2671561a7c3b5bcfbf1
Shebang123 <1160429973@qq.com>
2021-04-26 00:42:45 +0800

    update

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,22 +1,18 @@
-import pandas as pd
-
-def Get_Map_Information(file):
-    data = pd.read_csv(file)
-    data = data.values.tolist()
-    length = len(data)
-    number = [None] * length
-    coordinate = [None] * length
-    food_or_bonfire = [None] * length
-    supply = [None] * length
-    for i in range(length):
-        number[i] = i
-        coordinate[i] = data[i][1:4]
-        food_or_bonfire[i] = int(data[i][4])
-        supply[i] = int(data[i][5])
-    return number, coordinate, food_or_bonfire, supply
-
-class Map:
-    number, coordinate, food_or_bonfire, torch = Get_Map_Information("data.csv")
-
-print(Map.number[0])
-print(Map.coordinate[1])
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+mpl.rcParams['legend.fontsize'] = 10
+ 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z ** 2 + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+ax.plot(x, y, z, label='parametric curve')
+ax.legend()
+ 
+plt.show()
--- test.py
+++ test.py
@@ -26,7 +26,7 @@ class Survivor:
     comfortability = 10 #舒适度
     torch = 0 #火把数量,只在篝火点可以制作
 
-# 地图据点属性
+# 地图资源点属性
 class Map:
     # number表示序号，coordinate表示坐标
     # food_or_bonfire表示食物（1）或篝火（0），supply表示可提供的饱食度或舒适度
@@ -35,7 +35,8 @@ class Map:
     destination = coordinate[len(coordinate)-1] #终点
 
 # 描述路线
-def Draw_Roadmap():
+def Draw_Roadmap(*args):
+    
     print()
 
 if __name__ == "__main__":
