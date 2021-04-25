0d41fb937653f6d8291cf0a54b7f36ab2ed6edca
Shebang123 <1160429973@qq.com>
2021-04-25 16:28:50 +0800

    get information

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-
 file = "data.csv"
 data = pd.read_csv(file)
-
 list = data.values.tolist()
-print(list)
+length = len(list)
+coordinate = [None] * length
+food_or_bonfire = [None] * length
+supply = [None] * length
+for i in range(length):
+    coordinate[i] = list[i][1:4]
+    food_or_bonfire[i] = int(list[i][4])
+    supply[i] = int(list[i][5])
