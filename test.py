29d174ec95bf302b2d39cd64f834bef842e6a1cf
Shebang123 <1160429973@qq.com>
2021-04-26 21:55:36 +0800

    画图和一些简单修改

--- test.py
+++ test.py
@@ -1,37 +1,37 @@
 import pandas as pd
-import draw
-
-#获取地图坐标、食物和篝火信息
-def Get_Map_Information(file):
-    data = pd.read_csv(file)
-    data = data.values.tolist()
-    length = len(data)
-    number = [None] * length
-    coordinate = [None] * length
-    food_or_bonfire = [None] * length
-    supply = [None] * length
-    for i in range(length):
-        number[i] = i
-        coordinate[i] = data[i][1:4]
-        food_or_bonfire[i] = int(data[i][4])
-        supply[i] = int(data[i][5])
-    return number, coordinate, food_or_bonfire, supply
+import tools
 
 # 求生者属性
 class Survivor:
-    state = 1 #1表示生，0表示死
-    satiety = 10 #饱食度
-    comfortability = 10 #舒适度
-    torch = 0 #火把数量,只在篝火点可以制作
+    state = 1  # 1表示生，0表示死
+    satiety = 10  # 饱食度
+    comfortability = 10  # 舒适度
+    torch = 0  # 火把数量,只在篝火点可以制作
 
 # 地图资源点属性
 class Map:
     # number表示序号，coordinate表示坐标
     # food_or_bonfire表示食物（1）或篝火（0），supply表示可提供的饱食度或舒适度
-    number, coordinate, food_or_bonfire, supply = Get_Map_Information("data.csv")
-    origin = coordinate[0] # 起点
-    destination = coordinate[len(coordinate)-1] #终点
+    # 共293个食物资源点，可补给1011点饱食度；共294个篝火资源点，可补给880舒适度
+    number, coordinate, food_or_bonfire, supply = tools.Get_Map_Information("data.csv")
+    origin = coordinate[0]  # 起点
+    destination = coordinate[len(coordinate)-1]  # 终点
+
 
 if __name__ == "__main__":
-    print(Map.destination)
-    draw.Draw_Roadmap("data.csv")
+    # print(Map.destination)
+    # draw.Draw_Roadmap("data.csv")
+    # food = 0
+    # bonfire = 0
+    # count_food = 0
+    # count_bonfire = 0
+    # for i in range(len(Map.number)):
+    #     if Map.food_or_bonfire[i] == 1:
+    #         food += Map.supply[i]
+    #         count_food += 1
+    #     else:
+    #         bonfire += Map.supply[i]
+    #         count_bonfire += 1
+    # print("food:", count_food, food)
+    # print("bonfire:", count_bonfire, bonfire)
+    tools.Draw_Resource_Node("data.csv")
---
+++ tools.py
@@ -0,0 +1,80 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+
+# 获取地图坐标、食物和篝火信息
+def Get_Map_Information(file):
+    data = pd.read_csv(file)
+    data = data.values.tolist()
+    length = len(data)
+    number = [None] * length
+    coordinate = [None] * length
+    food_or_bonfire = [None] * length
+    supply = [None] * length
+    
+    for i in range(length):
+        number[i] = i
+        coordinate[i] = data[i][1:4]
+        food_or_bonfire[i] = int(data[i][4])
+        supply[i] = int(data[i][5])
+    
+    return number, coordinate, food_or_bonfire, supply
+
+# 获取文件中参数值
+def Get_XYZ(file):  
+    # coordinate = Get_Map_Information(file)[1]
+    # cow_X = [X[0] for X in coordinate]
+    # cow_Y = [Y[0] for Y in coordinate]
+    # cow_Z = [Z[0] for Z in coordinate]
+    # print(cow_X)
+    cow_X = pd.read_csv(file, usecols=[1])
+    cow_Y = pd.read_csv(file, usecols=[2])
+    cow_Z = pd.read_csv(file, usecols=[3])
+    cow_X = cow_X.values.tolist()
+    cow_Y = cow_Y.values.tolist()
+    cow_Z = cow_Z.values.tolist()
+    return cow_X, cow_Y, cow_Z
+
+# 画出所有资源点分布
+def Draw_Resource_Node(file):
+    choice = input("平面选0，空间选1:\n")
+    file = "data.csv"
+    cow_X, cow_Y, cow_Z = Get_XYZ(file)
+    food_or_bonfire = Get_Map_Information(file)[2]
+    length = len(cow_X)
+    food_X = food_Y = food_Z = bonfire_X = bonfire_Y = bonfire_Z = []
+
+    # 区分食物和篝火资源点
+    for i in range(length):
+        if food_or_bonfire[i] == 1:
+            food_X = food_X + cow_X[i]
+            food_Y = food_Y + cow_Y[i]
+            food_Z = food_Z + cow_Z[i]
+        elif food_or_bonfire[i] == 0:
+            bonfire_X = bonfire_X + cow_X[i]
+            bonfire_Y = bonfire_Y + cow_Y[i]
+            bonfire_Z = bonfire_Z + cow_Z[i]
+
+    # 空间
+    if choice == '1':
+        ax = plt.subplot(projection="3d")
+        ax.scatter(food_X, food_Y, food_Z, c = 'r')
+        ax.scatter(bonfire_X, bonfire_Y, bonfire_Z, c = 'b')
+        ax.set_xlabel('X')  # 设置x坐标轴
+        ax.set_ylabel('Y')  # 设置y坐标轴
+        ax.set_zlabel('Z')  # 设置z坐标轴
+        plt.show()
+    
+    # 平面
+    elif choice == '0':
+        ax = plt.subplot()
+        # fig = plt.figure()
+        # ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(food_X, food_Y, c = 'r')
+        ax.scatter(bonfire_X, bonfire_Y, c = 'b')
+        ax.set_xlabel('X')  # 设置x坐标轴
+        ax.set_ylabel('Y')  # 设置y坐标轴
+        plt.show()
+
+# 画出求生者从起点到终点的路线图
