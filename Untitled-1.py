4a9cfbaf3ce2293c6bcdf136c059391eee8044f1
Shebang123 <1160429973@qq.com>
2021-05-02 22:52:13 +0800

    word

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,18 +1,25 @@
-import matplotlib as mpl
+import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
  
-mpl.rcParams['legend.fontsize'] = 10
- 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-z = np.linspace(-2, 2, 100)
-r = z ** 2 + 1
-x = r * np.sin(theta)
-y = r * np.cos(theta)
-ax.plot(x, y, z, label='parametric curve')
-ax.legend()
- 
-plt.show()
+def Get_Map_Information(file):
+    data = pd.read_csv(file)
+    df = data['X']
+    print(df)
+    # data = data.values.tolist()
+    # length = len(data)
+    # number = [None] * length
+    # coordinate = [None] * length
+    # food_or_bonfire = [None] * length
+    # supply = [None] * length
+    
+    # for i in range(length):
+    #     number[i] = i
+    #     coordinate[i] = data[i][1:4]
+    #     food_or_bonfire[i] = int(data[i][4])
+    #     supply[i] = int(data[i][5])
+    
+    # return number, coordinate, food_or_bonfire, supply
+
+print(Get_Map_Information("data.csv"))
